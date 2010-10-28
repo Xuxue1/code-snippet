906902af1360c3f2de28042a62ea3c2033804013
James Du <peng1299.du@gmail.com>
2010-10-28 15:43:06 +0800

    add device config
    Signed-off-by: James Du <peng1299.du@gmail.com>

--- Device/DevChannel.h
+++ Device/DevChannel.h
@@ -3,13 +3,14 @@ class CDevChannel
 pubilc:
     CDevChannel(int nChannel, CDevice *device);
     ~CDevChannel();
+    CDevice *GetDevice();
     virtual IDevPreview *GetPreview(HWND);
     virtual IDevCtlPtz  *GetCtlPtz();
     virtual IDevAlarm   *GetAlarm();
 private:
     BOOL CreatePreview();
     int m_nChannel;
-    CDevice *m_Device;
+    CDevice     *m_Device;
     IDevPreview *m_Preview;
     IDevCtlPtz  *m_CtlPtz;
     IDevAlarm   *m_Alarm;
--- Device/Device.h
+++ Device/Device.h
@@ -3,12 +3,14 @@ class CDevice
 {
 public:
     CDevice(IDevUserLogin *usrLogIn, int type);
-    OpenChannel();
-    CloseChannel();
+    OpenChannel(unsigned int uChannelID);
+    CloseChannel(unsigned int uChannelID);
     AddChannel();
     RevomeChannel();
 private:
     int m_nType;
     std::vector<unsigned int> m_uChannelId;
+    std::deque<CDevChannel *> m_QChannels;
+    CDevConfig    *m_DevConfig;
     IDevUserLogin *m_DevUserLogin;
 };
